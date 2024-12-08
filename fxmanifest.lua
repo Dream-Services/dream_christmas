24fe5adc7027ff9e0801d18a682b55f9d1d00a4a
Tuncion Helper <41898282+github-actions[bot]@users.noreply.github.com>
2024-12-08 16:05:49 +0000

    chore: Updated manifest data for release 1.0.6.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,9 +6,9 @@ lua54 'yes'
 
 author 'Dream Services | Tuncion'
 description 'https://discord.gg/zppUXj4JRm'
-version '1.0.6'
-patch '#47'
-released '08.12.2024, 15:31 by Tuncion'
+version '1.0.6.1'
+patch '#51'
+released '08.12.2024, 17:05 by Tuncion'
 
 ui_page 'web/index.html'
 
